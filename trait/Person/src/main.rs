6afd25cb29d43ccfe5efa408e04aef565c95d420
coke-jiyong <qkrwldyd488@naver.com>
2024-09-13 14:59:16 +0900

    202040913

--- trait/Person/src/main.rs
+++ trait/Person/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 trait Printable {
     type Age;
     fn print(&self);
@@ -28,12 +30,26 @@ impl Printable for Person {
     }
 }
 
+impl fmt::Display for Person {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} is {} years old.",
+            self.name, self.age
+        )
+    }
+}
+#[derive(Debug,Clone, Default)] //Default : 구조체 각필드를 디폴트값으로 초기화
 struct Book {
     title: String,
     author: String,
     published: u32,
 }
-
+impl PartialEq for Book {
+    fn eq(&self, other: &Self) -> bool {
+        self.title == other.title && self.author == other.author
+    }
+}
 impl Printable for Book {
     type Age = u32;
     fn print(&self) {
@@ -47,18 +63,37 @@ impl Printable for Book {
     }
 }
 
+// impl Clone for Book {
+//     fn clone(&self) -> Self {
+//         Book {
+//             title: self.title.clone(),
+//             author: self.author.clone(),
+//             published: self.published,
+//         }
+//     }
+// }
+
 fn print_info(item: &dyn Printable<Age = u32>) {
     item.print();
 }
 
 fn main() {
-    let person = Person::new("Alice", 22);
-    let book = Book {
-        title: String::from("The Rust Programming Language"),
-        author: String::from("Steve Klabnik and Carol Nichols"),
-        published: 20230228,
-    };
+    // let person = Person::new("Alice", 22);
+    // let book = Book {
+    //     title: String::from("The Rust Programming Language"),
+    //     author: String::from("Steve Klabnik and Carol Nichols"),
+    //     published: 20230228,
+    // };
 
-    print_info(&person);
-    print_info(&book);
+    // print_info(&person);
+    // print_info(&book);
+
+    // println!("{}", person);
+    // println!("{:?}", book);
+
+    let book = Book::default();
+    let mut book_clone = book.clone();
+    println!("{:?}", book_clone);
+
+
 }
